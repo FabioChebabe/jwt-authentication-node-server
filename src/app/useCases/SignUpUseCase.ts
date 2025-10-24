3a27e1baba758350813edcb672736de9f44042da
Fabio Chebabe Carnelli <fabioccarnelli@gmail.com>
2025-10-23 23:09:16 -0300

    feat: create authorization with jwt

---
+++ src/app/useCases/SignUpUseCase.ts
@@ -0,0 +1,35 @@
+import { hash } from "bcryptjs";
+import { AccountAlreadyExists } from "../errors/AccountAlreadyExists";
+import { prismaClient } from "../libs/prismaClient";
+
+interface IInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type IOutput = void;
+
+export class SignUpUseCase {
+  async execute({ email, name, password }: IInput): Promise<IOutput> {
+    const accountAlreadyExist = await prismaClient.account.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (accountAlreadyExist) {
+      throw new AccountAlreadyExists();
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await prismaClient.account.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+  }
+}
